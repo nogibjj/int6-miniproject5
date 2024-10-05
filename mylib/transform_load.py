ed983d70789acef99619a1460507aaf1ed70ce7a
bella-tang <79435936+bella-tang@users.noreply.github.com>
2024-10-05 17:36:41 +0000

    modified execute to create table

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,26 +1,48 @@
-"""
-Transforms and Loads data into the local SQLite3 database
-Example:
-,general name,count_products,ingred_FPro,avg_FPro_products,avg_distance_root,ingred_normalization_term,semantic_tree_name,semantic_tree_node
-"""
 import sqlite3
-import csv
 import os
+import pandas as pd
 
-#load the csv file and insert into a new sqlite3 database
-def load(dataset="/workspaces/sqlite-lab/data/GroceryDB_IgFPro.csv"):
-    """"Transforms and Loads data into the local SQLite3 database"""
+# load the csv file and insert into a new sqlite3 database
+def load(dataset="data/Nutrition.csv"):
+    """Transforms and Loads data into the local SQLite3 database"""
 
-    #prints the full working directory and path
-    print(os.getcwd())
-    payload = csv.reader(open(dataset, newline=''), delimiter=',')
-    conn = sqlite3.connect('GroceryDB.db')
+    # prints the full working directory and path
+    print(f"Current working directory: {os.getcwd()}")
+    df = pd.read_csv(dataset)
+
+    # connect to the SQLite database
+    conn = sqlite3.connect('Nutrition.db')
     c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS GroceryDB")
-    c.execute("CREATE TABLE GroceryDB (id,general_name, count_products, ingred_FPro, avg_FPro_products, avg_distance_root, ingred_normalization_term, semantic_tree_name, semantic_tree_node)")
-    #insert
-    c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
+    c.execute("DROP TABLE IF EXISTS Nutrition")
+
+    # create table
+    c.execute('''
+        CREATE TABLE Nutrition (
+            ID INTEGER PRIMARY KEY,
+            cancer TEXT,
+            diabetes TEXT,
+            heart_disease TEXT,
+            EGGSFREQ INTEGER,
+            GREENSALADFREQ INTEGER,
+            FRIESFREQ INTEGER,
+            MILKFREQ INTEGER,
+            SODAFREQ INTEGER,
+            COFFEEFREQ INTEGER,
+            CAKESFREQ INTEGER
+        )
+    ''')
+
+    # insert
+    data_to_insert = df.values.tolist()
+    c.executemany('''
+        INSERT INTO Nutrition (
+            ID, cancer, diabetes, heart_disease, 
+                  EGGSFREQ, GREENSALADFREQ, FRIESFREQ, 
+                  MILKFREQ, SODAFREQ, COFFEEFREQ, CAKESFREQ
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', data_to_insert)
+
     conn.commit()
     conn.close()
-    return "GroceryDB.db"
 
+    return "Nutrition.db"
