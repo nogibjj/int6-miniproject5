9c75e8a4fd0edbd1e6963f91263ac783902a1c79
bella-tang <79435936+bella-tang@users.noreply.github.com>
2024-10-05 17:37:53 +0000

    changed import

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ ETL-Query script
 
 from mylib.extract import extract
 from mylib.transform_load import load
-from mylib.query import query
+from mylib.query import run_crud_operations
 
 # Extract
 print("Extracting data...")
@@ -16,4 +16,4 @@ load()
 
 # Query
 print("Querying data...")
-query()
+run_crud_operations()
